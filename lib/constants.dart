341163fe1787efe12e2d53968bc20f926da5d890
mavvy22 <mavvystudio@gmail.com>
2023-11-01 03:40:45 +0800

    add gql to global constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,19 @@
 const kStorageKey = 'tasknaut.json';
 const kStorageItemName = 'token';
+
+const kMyProfileGql = """
+   query myProfile {
+     myProfile {
+       id
+       username
+     }
+   }
+""";
+
+const kLoginGql = """
+  mutation login(\$input: LoginInput!) {
+    login(input: \$input) {
+      token
+    }
+  }
+""";
